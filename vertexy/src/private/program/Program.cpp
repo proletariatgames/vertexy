f33aef149e4bce1ccdd5132b9c74706e83b27ae9
Dan Ogles <dan@proletariat.com>
2022-04-26 13:49:06 -0400

    Cleanups

--- vertexy/src/private/program/Program.cpp
+++ vertexy/src/private/program/Program.cpp
@@ -1,6 +1,8 @@
 // Copyright Proletariat, Inc. All Rights Reserved.
 #include "program/Program.h"
 
+#include "program/ProgramDSL.h"
+
 using namespace Vertexy;
 
 ProgramInstanceBase* Program::s_currentInstance = nullptr;
@@ -12,4 +14,22 @@ ProgramParameter::ProgramParameter(const wchar_t* name)
 {
     vxy_assert_msg(Program::getCurrentInstance() != nullptr, "Cannot define a ProgramParameter outside of a Program::define block!");
     m_uid = Program::allocateParameterUID();
+}
+
+void Program::disallow(ProgramBodyTerm&& body)
+{
+    return disallow(ProgramBodyTerms(forward<ProgramBodyTerm>(body)));
+}
+
+void Program::disallow(ProgramBodyTerms&& body)
+{
+    vxy_assert_msg(Program::getCurrentInstance() != nullptr, "Cannot specify rules outside of a Program::define block!");
+    vector<UTerm> terms;
+    auto rule = make_unique<RuleStatement>(nullptr, move(body.terms));
+    s_currentInstance->addRule(move(rule));
+}
+
+Formula<1> Program::range(int min, int max)
+{
+    return Formula<1>(L"range");
 }
--- vertexy/src/public/program/Program.h
+++ vertexy/src/public/program/Program.h
@@ -4,6 +4,7 @@
 
 #include "ConstraintTypes.h"
 #include "Terms.h"
+
 // for non-eastl std::function
 #include <functional>
 
@@ -13,10 +14,17 @@
 namespace Vertexy
 {
 
+class ProgramBodyTerms;
+class ProgramBodyTerm;
+template<int ARITY> class Formula;
+template<int ARITY> class FormulaResult;
+
 // note: we use std::function here, because eastl::function doesn't provide C++17 deduction guides, which makes
 // it so we can't pass in lambdas directly.
 template<typename R, typename... ARGS>
 using ProgramDefinitionFunctor = std::function<R(ARGS...)>;
+
+template<typename R, typename... ARGS> class ProgramDefinition;
 
 // Returns a function type that returns R and takes N ARG arguments
 template<size_t N, typename R, typename ARG, typename... ARGS>
@@ -24,19 +32,14 @@ struct ArgumentRepeater { using type = typename ArgumentRepeater<N-1,R,ARG,ARG,A
 template<typename R, typename ARG, typename... ARGS>
 struct ArgumentRepeater<0, R, ARG, ARGS...> { using type = R(ARGS...); };
 
-template<int ARITY> class Formula;
-template<int ARITY> class FormulaResult;
-
-class ProgramFunctionTerm;
-class ProgramHeadTerm;
-class ProgramBodyTerm;
-class ProgramBodyTerms;
-
+// Synthesizes the FormulaResult::bind() callback function's signature.
 template<size_t SIZE>
-struct FormulaBinder {
-    using type =  std::function<typename ArgumentRepeater<SIZE, void, const ProgramSymbol&>::type>;
+struct FormulaBinder
+{
+    using type = std::function<typename ArgumentRepeater<SIZE, void, const ProgramSymbol&>::type>;
 };
 
+// Abstract base to call a function passed into FormulaResult::bind with a vector of arguments.
 class BindCaller
 {
 public:
@@ -44,6 +47,7 @@ public:
     virtual void call(const vector<ProgramSymbol>& syms) = 0;
 };
 
+// Implementation of BindCaller for a given arity
 template<size_t ARITY>
 class TBindCaller : public BindCaller
 {
@@ -59,6 +63,7 @@ public:
         std::apply(m_bindFun, zip(syms));
     }
 
+    // zip up ARITY elements from the vector into a tuple
     static auto zip(const vector<ProgramSymbol>& syms)
     {
         return zipper<0>(std::tuple<>(), syms);
@@ -73,13 +78,14 @@ public:
         }
         else
         {
-            return zipper<I+1>(std::tuple_cat(t, std::tuple(syms[I])), syms);
+            return zipper<I+1>(std::tuple_cat(eastl::move(t), std::tuple(syms[I])), syms);
         }
     }
 
     typename FormulaBinder<ARITY>::type m_bindFun;
 };
 
+// Base class for instantiated programs (once they have been given their arguments)
 class ProgramInstanceBase
 {
 public:
@@ -101,17 +107,18 @@ protected:
     vector<URuleStatement> m_ruleStatements;
 };
 
+// ProgramInstance returned from applying a ProgramDefinition (via the () operator)
 template<typename RESULT>
 class ProgramInstance : public ProgramInstanceBase
 {
 public:
     ProgramInstance() {}
 
+    // The typed result returned from the definition
     RESULT result;
 };
 
-template<typename R, typename... ARGS> class ProgramDefinition;
-
+// Static functions for defining rule programs
 class Program
 {
 protected:
@@ -119,6 +126,9 @@ protected:
     static int s_nextFormulaUID;
     static int s_nextParameterUID;
 public:
+    Program() = delete;
+    ~Program() = delete;
+
     static ProgramInstanceBase* getCurrentInstance() { return s_currentInstance; }
 
     template<typename R, typename... ARGS>
@@ -148,7 +158,6 @@ public:
     }
 
     static void disallow(ProgramBodyTerm&& body);
-
     static void disallow(ProgramBodyTerms&& body);
 
     static Formula<1> range(int min, int max);
@@ -157,302 +166,8 @@ public:
     static int allocateParameterUID() { return s_nextParameterUID++; }
 };
 
-class ProgramBinaryOpArgument
-{
-public:
-    ProgramBinaryOpArgument(int constant)
-    {
-        term = make_unique<SymbolTerm>(ProgramSymbol(constant));
-    }
-    ProgramBinaryOpArgument(const ProgramSymbol& sym)
-    {
-        term = make_unique<SymbolTerm>(sym);
-    }
-    ProgramBinaryOpArgument(ProgramParameter param)
-    {
-        term = make_unique<VariableTerm>(param);
-    }
-
-    explicit ProgramBinaryOpArgument(UTerm&& term) : term(move(term))
-    {
-    }
-
-    UTerm term;
-};
-
-
-// formula with terms applied to each argument
-class ProgramFunctionTerm
-{
-public:
-    explicit ProgramFunctionTerm(int uid, const wchar_t* name, vector<ProgramBodyTerm>&& args) : uid(uid), name(name), args(move(args)) {}
-    int uid;
-    const wchar_t* name;
-    vector<ProgramBodyTerm> args;
-};
-
-class ProgramBodyTerm
-{
-protected:
-    ProgramBodyTerm();
-
-public:
-    ProgramBodyTerm(int constant)
-    {
-        term = make_unique<SymbolTerm>(ProgramSymbol(constant));
-    }
-    ProgramBodyTerm(const ProgramSymbol& s)
-    {
-        term = make_unique<SymbolTerm>(s);
-    }
-    ProgramBodyTerm(const ProgramParameter& p)
-    {
-        term = make_unique<VariableTerm>(p);
-    }
-    ProgramBodyTerm(ProgramFunctionTerm&& f)
-    {
-        vector<UTerm> argTerms;
-        argTerms.reserve(f.args.size());
-
-        for (ProgramBodyTerm& arg : f.args)
-        {
-            argTerms.push_back(move(arg.term));
-        }
-        term = make_unique<FunctionTerm>(f.uid, f.name, move(argTerms));
-    }
-    ProgramBodyTerm(ProgramBinaryOpArgument&& h)
-    {
-        term = move(h.term);
-    }
-
-    explicit ProgramBodyTerm(UTerm&& inTerm)
-    {
-        term = move(inTerm);
-    }
-
-    UTerm term;
-};
-
-class ProgramBodyTerms
-{
-public:
-    explicit ProgramBodyTerms(vector<UTerm>&& inTerms) : terms(move(inTerms))
-    {
-    };
-    ProgramBodyTerms(ProgramBodyTerm&& rhs)
-    {
-        terms.push_back(move(rhs.term));
-    }
-    void add(UTerm&& child)
-    {
-        terms.push_back(move(child));
-    }
-
-    vector<UTerm> terms;
-};
-
-
-inline ProgramBodyTerms operator&&(ProgramBodyTerm&& lhs, ProgramBodyTerm&& rhs)
-{
-    vector<UTerm> terms;
-    terms.reserve(2);
-    terms.push_back(move(lhs.term));
-    terms.push_back(move(rhs.term));
-    return ProgramBodyTerms(move(terms));
-}
-
-inline ProgramBodyTerms operator&&(ProgramBodyTerms&& lhs, ProgramBodyTerm&& rhs)
-{
-    vector<UTerm> terms = move(lhs.terms);
-    terms.push_back(move(rhs.term));
-    return ProgramBodyTerms(move(terms));
-}
-
-inline ProgramBodyTerm operator~(ProgramBodyTerm&& rhs)
-{
-    UTerm term = make_unique<UnaryOpTerm>(EUnaryOperatorType::Negate, move(rhs.term));
-    return ProgramBodyTerm(move(term));
-}
-
-inline ProgramBinaryOpArgument operator<(ProgramBinaryOpArgument&& lhs, ProgramBinaryOpArgument&& rhs)
-{
-    UTerm term = make_unique<BinaryOpTerm>(EBinaryOperatorType::LessThan, move(lhs.term), move(rhs.term));
-    return ProgramBinaryOpArgument(move(term));
-}
-
-inline ProgramBinaryOpArgument operator<=(ProgramBinaryOpArgument&& lhs, ProgramBinaryOpArgument&& rhs)
-{
-    UTerm term = make_unique<BinaryOpTerm>(EBinaryOperatorType::LessThanEq, move(lhs.term), move(rhs.term));
-    return ProgramBinaryOpArgument(move(term));
-}
-inline ProgramBinaryOpArgument operator>(ProgramBinaryOpArgument&& lhs, ProgramBinaryOpArgument&& rhs)
-{
-    UTerm term = make_unique<BinaryOpTerm>(EBinaryOperatorType::GreaterThan, move(lhs.term), move(rhs.term));
-    return ProgramBinaryOpArgument(move(term));
-}
-inline ProgramBinaryOpArgument operator>=(ProgramBinaryOpArgument&& lhs, ProgramBinaryOpArgument&& rhs)
-{
-    UTerm term = make_unique<BinaryOpTerm>(EBinaryOperatorType::GreaterThanEq, move(lhs.term), move(rhs.term));
-    return ProgramBinaryOpArgument(move(term));
-}
-inline ProgramBinaryOpArgument operator==(ProgramBinaryOpArgument&& lhs, ProgramBinaryOpArgument&& rhs)
-{
-    UTerm term = make_unique<BinaryOpTerm>(EBinaryOperatorType::Equality, move(lhs.term), move(rhs.term));
-    return ProgramBinaryOpArgument(move(term));
-}
-inline ProgramBinaryOpArgument operator!=(ProgramBinaryOpArgument&& lhs, ProgramBinaryOpArgument&& rhs)
-{
-    UTerm term = make_unique<BinaryOpTerm>(EBinaryOperatorType::Inequality, move(lhs.term), move(rhs.term));
-    return ProgramBinaryOpArgument(move(term));
-}
-inline ProgramBinaryOpArgument operator*(ProgramBinaryOpArgument&& lhs, ProgramBinaryOpArgument&& rhs)
-{
-    UTerm term = make_unique<BinaryOpTerm>(EBinaryOperatorType::Multiply, move(lhs.term), move(rhs.term));
-    return ProgramBinaryOpArgument(move(term));
-}
-inline ProgramBinaryOpArgument operator/(ProgramBinaryOpArgument&& lhs, ProgramBinaryOpArgument&& rhs)
-{
-    UTerm term = make_unique<BinaryOpTerm>(EBinaryOperatorType::Divide, move(lhs.term), move(rhs.term));
-    return ProgramBinaryOpArgument(move(term));
-}
-inline ProgramBinaryOpArgument operator+(ProgramBinaryOpArgument&& lhs, ProgramBinaryOpArgument&& rhs)
-{
-    UTerm term = make_unique<BinaryOpTerm>(EBinaryOperatorType::Add, move(lhs.term), move(rhs.term));
-    return ProgramBinaryOpArgument(move(term));
-}
-inline ProgramBinaryOpArgument operator-(ProgramBinaryOpArgument&& lhs, ProgramBinaryOpArgument&& rhs)
-{
-    UTerm term = make_unique<BinaryOpTerm>(EBinaryOperatorType::Subtract, move(lhs.term), move(rhs.term));
-    return ProgramBinaryOpArgument(move(term));
-}
-
-class ProgramHeadTerm
-{
-public:
-    ProgramHeadTerm(ProgramFunctionTerm&& f)
-    {
-        vector<UTerm> argTerms;
-        argTerms.reserve(f.args.size());
-
-        for (ProgramBodyTerm& arg : f.args)
-        {
-            argTerms.push_back(move(arg.term));
-        }
-        term = make_unique<FunctionTerm>(f.uid, f.name, move(argTerms));
-    }
-
-    explicit ProgramHeadTerm(UTerm&& term) : term(move(term))
-    {
-    }
-
-    UTerm term;
-};
-
-class ProgramHeadDisjunctionTerm : public ProgramHeadTerm
-{
-public:
-    explicit ProgramHeadDisjunctionTerm(unique_ptr<DisjunctionTerm>&& term) : ProgramHeadTerm(move(term)) {}
-    void add(UTerm&& child)
-    {
-        static_cast<DisjunctionTerm*>(term.get())->children.push_back(move(child));
-    }
-};
-
-inline ProgramHeadDisjunctionTerm operator|(ProgramHeadTerm&& lhs, ProgramHeadTerm&& rhs)
-{
-    vector<UTerm> children;
-    children.reserve(2);
-    children.push_back(move(lhs.term));
-    children.push_back(move(rhs.term));
-    return ProgramHeadDisjunctionTerm(make_unique<DisjunctionTerm>(move(children)));
-}
-inline ProgramHeadDisjunctionTerm& operator|(ProgramHeadDisjunctionTerm& lhs, ProgramHeadTerm&& rhs)
-{
-    lhs.add(move(rhs.term));
-    return lhs;
-}
-
-inline void operator<<=(ProgramHeadTerm&& head, ProgramBodyTerms&& body)
-{
-    vxy_assert_msg(Program::getCurrentInstance() != nullptr, "Cannot specify rules outside of a Program::define block!");
-    auto rule = make_unique<RuleStatement>(move(head.term), move(body.terms));
-    Program::getCurrentInstance()->addRule(move(rule));
-}
-
-inline void operator<<=(ProgramHeadTerm&& head, ProgramBodyTerm&& body)
-{
-    vector<UTerm> terms;
-    terms.push_back(move(body.term));
-    operator<<=(forward<ProgramHeadTerm>(head), ProgramBodyTerms(move(terms)));
-}
-
-template<int ARITY>
-class Formula
-{
-    friend FormulaResult;
-
-public:
-    Formula(const wchar_t* name=nullptr)
-        : m_name(name)
-    {
-        vxy_assert_msg(Program::getCurrentInstance() != nullptr, "Cannot create a Formula outside of a Program::define block!");
-        m_uid = Program::allocateFormulaUID();
-    }
-
-    template<typename... ARGS>
-    ProgramFunctionTerm operator()(ARGS&&... args)
-    {
-        static_assert(sizeof...(args) == ARITY, "Wrong number of arguments for formula");
-        vector<ProgramBodyTerm> fargs;
-        fargs.reserve(ARITY);
-
-        foldArgs(fargs, args...);
-        return ProgramFunctionTerm(m_uid, m_name, move(fargs));
-    }
-
-private:
-    template<typename T, typename... REM>
-    void foldArgs(vector<ProgramBodyTerm>& outArgs, T&& arg, REM&&... rem)
-    {
-        outArgs.push_back(move(arg));
-        foldArgs(outArgs, rem...);
-    }
-    void foldArgs(vector<ProgramBodyTerm>& outArgs) {}
-
-    const wchar_t* m_name;
-    int m_uid;
-};
-
-template<int ARITY>
-class FormulaResult
-{
-public:
-    FormulaResult()
-    {
-        m_instance = nullptr;
-        m_formulaUID = -1;
-        m_formulaName = nullptr;
-    }
-
-    FormulaResult(const Formula<ARITY>& formula)
-    {
-        vxy_assert_msg(Program::getCurrentInstance() != nullptr, "Cannot construct a FormulaResult outside of a Program::define block!");
-        m_instance = Program::getCurrentInstance();
-        m_formulaName = formula.m_name;
-        m_formulaUID = formula.m_uid;
-    }
-
-    void bind(typename FormulaBinder<ARITY>::type&& binder)
-    {
-        vxy_assert_msg(m_instance && m_formulaUID >= 0, "FormulaResult not bound to a formula");
-        m_instance->addBinder(m_formulaUID, eastl::make_unique<TBindCaller<ARITY>>(eastl::move(binder)));
-    }
-
-    ProgramInstanceBase* m_instance;
-    const wchar_t* m_formulaName;
-    int m_formulaUID;
-};
-
+// ProgramDefinition is return from Program::define(). Contains the "code"
+// for the program, which can be turned into a ProgramInstance by specifying the program's arguments.
 template<typename R, typename... ARGS>
 class ProgramDefinition
 {
@@ -462,32 +177,19 @@ public:
     {
     }
 
-    unique_ptr<ProgramInstance<R>> operator()(ARGS&&... args) const
+    // "parse" the definition, returning the ProgramInstance.
+    inline unique_ptr<ProgramInstance<R>> apply(ARGS&&... args)
     {
         return Program::runDefinition<R, ARGS...>(m_definition, forward<ARGS>(args)...);
+    }
+
+    unique_ptr<ProgramInstance<R>> operator()(ARGS&&... args)
+    {
+        return apply(eastl::move(args)...);
     }
 
 protected:
     ProgramDefinitionFunctor<R, ARGS...> m_definition;
 };
-
-inline void Program::disallow(ProgramBodyTerm&& body)
-{
-    return disallow(ProgramBodyTerms(forward<ProgramBodyTerm>(body)));
-}
-
-inline void Program::disallow(ProgramBodyTerms&& body)
-{
-    vxy_assert_msg(Program::getCurrentInstance() != nullptr, "Cannot specify rules outside of a Program::define block!");
-    vector<UTerm> terms;
-    auto rule = make_unique<RuleStatement>(nullptr, move(body.terms));
-    s_currentInstance->addRule(move(rule));
-}
-
-inline Formula<1> Program::range(int min, int max)
-{
-    return Formula<1>(L"range");
-}
-
 
 }
