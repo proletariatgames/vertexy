cc097d9d984adc345cd8de4866c4f35d9ff45ebf
Dan Ogles <dan@proletariat.com>
2022-04-26 13:22:03 -0400

    - Add formula binding API - Add VXY_PARAMETER/VXY_FORMULA macros for named params/formulas (for debugging)

--- vertexy/src/private/program/Program.cpp
+++ vertexy/src/private/program/Program.cpp
@@ -3,11 +3,12 @@
 
 using namespace Vertexy;
 
-ProgramInstance* Program::s_currentInstance = nullptr;
+ProgramInstanceBase* Program::s_currentInstance = nullptr;
 int Program::s_nextFormulaUID = 1;
 int Program::s_nextParameterUID = 1;
 
-ProgramParameter::ProgramParameter()
+ProgramParameter::ProgramParameter(const wchar_t* name)
+    : m_name(name)
 {
     vxy_assert_msg(Program::getCurrentInstance() != nullptr, "Cannot define a ProgramParameter outside of a Program::define block!");
     m_uid = Program::allocateParameterUID();
