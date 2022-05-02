05ef38dd06b9b0ccf967cc0efbdb28a4adb4dd0b
prolecengelhard <72163502+prolecengelhard@users.noreply.github.com>
2022-05-02 14:36:14 -0400

    making the project

---
+++ vertexyWFC/src/WFC.cpp
@@ -0,0 +1,24 @@
+﻿// SolverTest.cpp : Defines the entry point for the application.
+//
+
+#include "WFC.h"
+
+#include <EATest/EATest.h>
+#include <EASTL/set.h>
+
+#include "ConstraintSolver.h"
+#include "util/Asserts.h"
+#include "ds/ValueBitset.h"
+#include "ds/ESTree.h"
+#include "topology/DigraphTopology.h"
+
+using namespace Vertexy;
+
+static constexpr int FORCE_SEED = 0;
+static constexpr char* INPUT_FILE = "SimpleInput.bmp";
+
+int main(int argc, char* argv[])
+{
+
+	
+}
